585788960c96b98489abd765f441054cbabaff17
khushalGrover <khushal15grover@gmail.com>
2024-10-17 13:21:39 +0530

    small changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,15 @@
-const Button = ({ addClass, children }) => {
+// Button.js
+import React from 'react';
 
-
-  
+const Button = ({ addClass, onClick, children }) => {
   return (
-    <button className={`bg-primary px-5 py-2 rounded-3xl text-white ` + addClass}>
+    <button
+    className={`bg-primary px-5 py-2 rounded-3xl text-white ` + addClass}
+      onClick={onClick}
+    >
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
