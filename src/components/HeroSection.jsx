81834c7ba2bc76cb33be32ca280a00cc5b49c5eb
khushalGrover <khushal15grover@gmail.com>
2024-10-29 23:17:22 +0530

    not responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,41 +1,28 @@
 import React from "react";
 import "./HeroSection.css"; // Import CSS file
 import Button from "./Button"; // Import Button component
+import ContactForm from "./ContactForm";
+
 
 const HeroSection = () => {
-  return (
-    <section className="hero-section">
-      <div className="hero-content">
-        <div className="hero-text ">
-          <h1 className="font-semibold text-4xl">Welcome to Our Website</h1>
-          <p>Your journey to excellence starts here. Let us help you achieve your dreams.</p>
-          <Button addClass="secondaryBtn">
-              Get Started
-            </Button>
-        </div>
-        <div className="contact-form">
-          <h2>Contact Us</h2>
-          <form>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input type="text" id="name" placeholder="Your Name" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" placeholder="Your Email" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea id="message" rows="4" placeholder="Your Message" required></textarea>
-            </div>
-            <Button>
-              Send Message
-            </Button>
-          </form>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="hero-section">
+			<div className="hero-content">
+				<div className="hero-text ">
+					<h1 className="font-semibold text-4xl">
+						Welcome to Our Websites
+					</h1>
+					<p>
+						Your journey to excellence starts here. Let us help you
+						achieve your dreams.
+					</p>
+					<Button>Get Started</Button>
+				</div>
+				
+				<ContactForm />
+			</div>
+		</section>
+	);
 };
 
 export default HeroSection;
--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,7 @@ import PopularHotelSection from "./PopularHotelSection"
 import HistorySection from "./About"
 import Signup from "./Signup/Signup"
 import Hero from "./Hero"
+import ContactForm from "./ContactForm"
 
-export { Navbar, HeroSection, FacilitiesSection, TestimonialSection, LocationSection, FooterSection, PopularHotelSection, HistorySection, Signup, Hero}
+export { Navbar, HeroSection, FacilitiesSection, TestimonialSection, LocationSection, FooterSection, PopularHotelSection, HistorySection, Signup, Hero, ContactForm}
 
