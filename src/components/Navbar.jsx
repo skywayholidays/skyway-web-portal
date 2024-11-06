86ff643546d8343faa1f1774206295d2e58e74d0
DrNKGupta <nidhish.cu@gmail.com>
2024-11-06 15:53:26 +0530

    dropdown menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,22 @@ const Navbar = ({ isAuthRoute }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   const [anchorEl, setAnchorEl] = useState(null);
+  const [internationalAnchorEl, setInternationalAnchorEl] = useState(null);
 
+  // Functions to handle main menu
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleMenuClose = () => {
     setAnchorEl(null);
+  };
+
+  // Functions to handle "Internationals" submenu
+  const handleInternationalMenuOpen = (event) => {
+    setInternationalAnchorEl(event.currentTarget);
+  };
+  const handleInternationalMenuClose = () => {
+    setInternationalAnchorEl(null);
   };
 
   return (
@@ -44,15 +53,49 @@ const Navbar = ({ isAuthRoute }) => {
             >
               <MenuItem onClick={handleMenuClose}><a href="/">Home</a></MenuItem>
               <MenuItem onClick={handleMenuClose}><a href="/about">About</a></MenuItem>
-              <MenuItem onClick={handleMenuClose}><a href="/thailand">Internationals</a></MenuItem>
+              <MenuItem onClick={handleInternationalMenuOpen}>Internationals</MenuItem>
               <MenuItem onClick={handleMenuClose}><a href="/contact">Contact</a></MenuItem>
+            </Menu>
+            {/* Submenu for Internationals */}
+            <Menu
+              anchorEl={internationalAnchorEl}
+              open={Boolean(internationalAnchorEl)}
+              onClose={handleInternationalMenuClose}
+              sx={{ ml: 2 }}
+            >
+              <MenuItem onClick={handleInternationalMenuClose}><a href="/thailand">Thailand</a></MenuItem>
+              <MenuItem onClick={handleInternationalMenuClose}><a href="/bali">Bali</a></MenuItem>
+              <MenuItem onClick={handleInternationalMenuClose}><a href="/dubai">Dubai</a></MenuItem>
+              <MenuItem onClick={handleInternationalMenuClose}><a href="/malaysia">Malaysia</a></MenuItem>
+              <MenuItem onClick={handleInternationalMenuClose}><a href="/maldivs">Maldivs</a></MenuItem>
+              <MenuItem onClick={handleInternationalMenuClose}><a href="/india">India</a></MenuItem>
             </Menu>
           </>
         ) : (
           <Box sx={{ display: 'flex', gap: 4 }} className="text-xl">
             <a href="/">Home</a>
             <a href="/about">About</a>
-            <a href="/thailand">Internationals</a>
+            <Box
+              onMouseEnter={handleInternationalMenuOpen}
+              onMouseLeave={handleInternationalMenuClose}
+              sx={{ position: 'relative', cursor: 'pointer' }}
+            >
+              <a href="#" onClick={(e) => e.preventDefault()}>Internationals</a>
+              <Menu
+                anchorEl={internationalAnchorEl}
+                open={Boolean(internationalAnchorEl)}
+                onClose={handleInternationalMenuClose}
+                MenuListProps={{ onMouseLeave: handleInternationalMenuClose }}
+                sx={{ mt: 2 }}
+              >
+                <MenuItem onClick={handleInternationalMenuClose}><a href="/thailand">Thailand</a></MenuItem>
+                <MenuItem onClick={handleInternationalMenuClose}><a href="/bali">Bali</a></MenuItem>
+                <MenuItem onClick={handleInternationalMenuClose}><a href="/dubai">Dubai</a></MenuItem>
+                <MenuItem onClick={handleInternationalMenuClose}><a href="/malaysia">Malaysia</a></MenuItem>
+                <MenuItem onClick={handleInternationalMenuClose}><a href="/maldivs">Maldivs</a></MenuItem>
+                <MenuItem onClick={handleInternationalMenuClose}><a href="/india">India</a></MenuItem>
+              </Menu>
+            </Box>
             <a href="/contact">Contact</a>
           </Box>
         )}
